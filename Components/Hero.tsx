e6ce91bebdeb5428314148038508122f541a1eac
m@nji <spontaneousmanji@gmail.com>
2023-09-08 07:14:05 +0100

    chore: updating app

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -9,7 +9,6 @@ const Hero = () => {
           <div className='flex flex-col justify-center w-full items-center md:items-start '>
             <h1 className='py-3 text-3xl md:text-4xl font-bold'>Electricity Billing System</h1>
             <p className='text-2xl'>Makes Billing Accessible and Simple</p>
-            <button className='bg-blue-600 text-white px-4 py-2 mt-4 rounded-md'>Subscribe</button>
           </div>
           <div>
             {/* <Image className='w-full h-[80%]' src={'/meter.jpg'} width={200} height={100} alt='/' /> */}
--- Components/Princing.tsx
+++ Components/Princing.tsx
@@ -13,13 +13,13 @@ const Princing = () => {
 
       <div className='max-w-[1240px] mx-auto py-12'>
             <div className='text-center py-8 text-slate-300'>
-                <h2 className='text-3xl uppercase'>Princing</h2>
+                {/* <h2 className='text-3xl uppercase'>Princing</h2> */}
                 <h3 className='text-5xl font-bold text-white py-8'>The Right Price for your Light</h3>
-                <p className='text-3xl'>This platforms exist to give out the easiest and flexible princing to access Light at the confort of your zone</p>
+                <p className='text-3xl'>This platforms exist to give out the easiest and flexible pricing to access Light at the comfort of your zone</p>
             </div>
       </div>
 
-      <div className='grid md:grid-cols-2'>
+      {/* <div className='grid md:grid-cols-2'>
           <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
             <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
             <div>
@@ -51,7 +51,7 @@ const Princing = () => {
               </div>
         </div>
 
-      </div>
+      </div> */}
     </div>
   );
 }
